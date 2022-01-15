e3893e546b488759c12c3be04704ab84c0cd90e4
md.rezwanur rahman <rezwanur@smartdatasoft.net>
2022-01-15 22:12:50 +0600

    added data for payment page

---
+++ lib/pdf_viewer/purchase_book/book_order.dart
@@ -0,0 +1,322 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class BookPaymentPage extends StatefulWidget {
+  const BookPaymentPage({Key? key}) : super(key: key);
+
+
+  @override
+  _BookPaymentPageState createState() => _BookPaymentPageState();
+
+}
+
+
+  class _BookPaymentPageState extends State<BookPaymentPage> {
+
+  String paymentNumbr="";
+
+  @override
+  Widget build(BuildContext context) {
+    String bookId = "1";
+    String mobileNumber = "017";
+    String transactionId = "jh2";
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Pay Now"),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                // margin: const EdgeInsets.all(10),
+                child: Text(
+                  'অর্ডার বিস্তারিত',
+                ),
+              ),
+              const Divider(),
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      'আইটেম সমূহ',
+                    ),
+                  ),
+                  const Spacer(),
+                  Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Text(
+                        'সর্বমোট',
+                      )),
+                ],
+              ),
+              const Divider(),
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      'বইয়ের নাম',
+                    ),
+                  ),
+                  const Spacer(),
+                  Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Text('৳250')),
+                ],
+              ),
+              const Divider(),
+              Row(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      'সর্বমোট',
+                    ),
+                  ),
+                  const Spacer(),
+                  Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      child: Text('৳500')),
+                ],
+              ),
+              const Divider(),
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      ' চার্জ',
+                    ),
+                  ),
+                  const Spacer(),
+                  Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 10),
+                      child: const Text('৳0')),
+                ],
+              ),
+              const Divider(),
+              Row(
+                children: [
+                  Container(
+                    margin: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Text(
+                      'সর্বমোট',
+                    ),
+                  ),
+                  const Spacer(),
+                  Container(
+                      margin: EdgeInsets.symmetric(horizontal: 10),
+                      child: Text(
+                        '৳60',
+                      )),
+                ],
+              ),
+              const Divider(),
+              const SizedBox(height: 20),
+              Container(child: paymentProcess(contx: context))
+            ],
+          ),
+        ));
+  }
+
+  Widget paymentProcess({BuildContext? contx}) {
+    return Container(
+      child: Column(
+        children: [
+          Container(
+              height: 50,
+              alignment: Alignment.center,
+              child: Text(
+                'পেমেন্ট পদ্ধতি',
+              )),
+          Container(
+            padding: EdgeInsets.all(5),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "> সর্বপ্রথম পেমেন্ট সম্পন্ন করুন (Send Money)",
+                ),
+                Text(
+                  "> নিচের ফর্মটি পূরণ করুন",
+                ),
+                Text(
+                  "> সাবমিট বাটনে ক্লিক করুন",
+                ),
+                Text(
+                  "> পেমেন্ট ও সবকিছু ঠিক থাকলে ২৪ ঘন্টার ভেতর সাবস্ক্রিপশন একটিভ হবে",
+                ),
+                Text(
+                  "> ২৪ ঘন্টার ভেতর একটিভ না হলে আমাদের সাথে যোগাযোগ করুন এপের যোগাযোগ পেইজ থেকে",
+                ),
+              ],
+            ),
+          ),
+          Row(
+            children: [
+              Container(
+                padding: EdgeInsets.all(5),
+                child: Text(
+                  "Send Money: 01713509349",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              IconButton(
+                  onPressed: () {
+                    Clipboard.setData(new ClipboardData(text: "01713509349"))
+                        .then((_) {
+                      ScaffoldMessenger.of(contx!).showSnackBar(
+                          SnackBar(content: Text("01713509349 Copied")));
+                    });
+                  },
+                  icon: Icon(
+                    Icons.content_copy_rounded,
+                    size: 30,
+                  )),
+            ],
+          ),
+          Container(
+            child: insertTransactionTxt(),
+          ),
+          ElevatedButton(onPressed: (){}, child: Text("SUBMIT"))
+        ],
+      ),
+    );
+  }
+
+  Widget insertTransactionTxt() {
+    return Container(
+      margin: EdgeInsets.only(left: 5),
+      child: Row(
+        children: [
+          Container(child: Text("Rocket Number:")),
+          SizedBox(
+            width: 10,
+          ),
+          Container(
+            width: 180,
+            child: TextFormField(
+              initialValue: "01",
+              keyboardType: TextInputType.number,
+              decoration: const InputDecoration(
+                hintText: "insert payment number",
+                border: OutlineInputBorder(),
+                alignLabelWithHint: true,
+                contentPadding: EdgeInsets.fromLTRB(10, 0, 0, 10),
+                enabled: true,
+              ),
+
+              onChanged: (val) {
+                setState(() {
+                  paymentNumbr = val;
+                });
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+/*                     child: TextFormField(
+
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Text('${widget.payment_name} number'),
+                  Flexible(
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 10, right: 10),
+                      child: TextFormField(
+                        onSaved: (newValue) {
+                          setState(() {
+                            phone_number = newValue;
+                          });
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "${widget.payment_name} number";
+                          }
+                        },
+                        onChanged: (value) {
+                          setState(() {
+                            phone_number = value;
+                          });
+                        },
+                        initialValue: "01",
+                        keyboardType: TextInputType.number,
+                        decoration: InputDecoration(
+                          hintText: '${widget.payment_name} number',
+                          border: const OutlineInputBorder(),
+                          alignLabelWithHint: true,
+                          contentPadding:
+                              const EdgeInsets.fromLTRB(10, 0, 0, 10),
+                          enabled: true,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Text('${widget.payment_name} Transaction ID'),
+                  Flexible(
+                    child: Container(
+                      margin: const EdgeInsets.only(left: 10, right: 10),
+                      child: TextFormField(
+                        onSaved: (newValue) {
+                          setState(() {
+                            transaction_Id = newValue;
+                          });
+                        },
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return "Enter your ${widget.payment_name} Transaction ID";
+                          }
+                        },
+                        onChanged: (value) {
+                          setState(() {
+                            transaction_Id = value;
+                          });
+                        },
+                        decoration: InputDecoration(
+                          hintText: '${widget.payment_name} Transaction ID',
+                          border: const OutlineInputBorder(),
+                          alignLabelWithHint: true,
+                          contentPadding:
+                              const EdgeInsets.fromLTRB(10, 0, 0, 10),
+                          enabled: true,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              alignment: Alignment.center,
+              child: payment.isloading
+                  ? const Center(
+                      child: CircularProgressIndicator(),
+                    )
+                  : ElevatedButton(
+                      onPressed: () {
+                        validationchack(context);
+                      },
+                      child: const Text('Submit')),
+
+            )
+          ],
+        ),
+      ),
+    );
+  }
+*/
+}
--- lib/pdf_viewer/purchase_book/payment_list.dart
+++ lib/pdf_viewer/purchase_book/payment_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:untitled/pdf_viewer/purchase_book/book_order.dart';
 
 class PaymentList extends StatelessWidget {
   const PaymentList({Key? key}) : super(key: key);
@@ -20,9 +21,115 @@ class PaymentList extends StatelessWidget {
       ),
       body: Column(
         children: [
-
+          paymentItemList(
+              onTap: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => BookPaymentPage()));
+              },
+              image: Image.asset(
+                'images/bikash.png',
+              ),
+              name: 'Bkash'),
+          paymentItemList(
+              image: Image.asset(
+                'images/rocket.jpg',
+              ),
+              name: 'Rocket')
         ],
       ),
     );
+  }
+
+  /*
+  {GestureTapCallback? onTap, Widget? imagechild, String? name}) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      height: MediaQuery.of(context).size.height * 0.08,
+      width: double.infinity,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(
+          10,
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.3),
+            spreadRadius: 5,
+            blurRadius: 7,
+            offset: Offset(0, 3), // changes position of shadow
+          ),
+        ],
+      ),
+      child: InkWell(
+        onTap: onTap,
+        child: Row(
+          children: [
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 80,
+                    child: imagechild),
+              ],
+            ),
+            const SizedBox(width: 10),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  width: 50,
+                  child: Text(
+                    name!,
+                    style: GoogleFonts.hindSiliguri(fontSize: 16, fontWeight: FontWeight.w600),
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  */
+  Widget paymentItemList(
+      {GestureTapCallback? onTap, Widget? image, String? name}) {
+    return Container(
+      height: 100,
+        margin: EdgeInsets.all(10),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                  color: Colors.grey.withOpacity(0.3),
+                  spreadRadius: 5,
+                  blurRadius: 7,
+                  offset: Offset(0, 3))
+            ]),
+        child: InkWell(
+          onTap: onTap,
+          child: Row(
+            children: [
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    height: 50,
+                    child: image,
+                  )
+                ],
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                    alignment: Alignment.center,
+                    child: Text(name.toString()),
+                  )
+                ],
+              )
+            ],
+          ),
+        ));
   }
 }
